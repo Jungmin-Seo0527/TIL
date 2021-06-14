b033d170383604b218662430549aac3493c1fa91
Jungmin-Seo0527 <soato1405@naver.com>
2021-06-14 12:46:09 +0900

    2-7. 주문과 할인 도메인 개발

---
+++ src/main/java/hello/core1/discount/FixDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core1.discount;
+
+import hello.core1.member.Grade;
+import hello.core1.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private int discountFixAmout = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmout;
+        } else {
+            return 0;
+        }
+    }
+}
