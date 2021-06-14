75ffd7d4a22d1db40a29ac3313015de51ccb046e
Jungmin-Seo0527 <soato1405@naver.com>
2021-06-14 12:02:17 +0900

    2-5. 회원 도메인 실행과 테스트

---
+++ src/test/java/hello/member/MemberServiceTest.java
@@ -0,0 +1,26 @@
+package hello.member;
+
+import hello.core1.member.Grade;
+import hello.core1.member.Member;
+import hello.core1.member.MemberService;
+import hello.core1.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+        // given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        // when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        // then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
