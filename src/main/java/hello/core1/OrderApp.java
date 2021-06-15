4665d0d52f38f599eb60839e694c3e792798ce5f
Jungmin-Seo0527 <soato1405@naver.com>
2021-06-15 12:45:40 +0900

    3-9. 스프링으로 전환하기

--- src/main/java/hello/core1/OrderApp.java
+++ src/main/java/hello/core1/OrderApp.java
@@ -5,12 +5,18 @@ import hello.core1.member.Member;
 import hello.core1.member.MemberService;
 import hello.core1.order.Order;
 import hello.core1.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        // AppConfig appConfig = new AppConfig();
+        // MemberService memberService = appConfig.memberService();
+        // OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
