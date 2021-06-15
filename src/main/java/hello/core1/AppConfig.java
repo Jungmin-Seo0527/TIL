4cce3de07aa30f2bfdbc11da33fac99585b9db16
Jungmin-Seo0527 <soato1405@naver.com>
2021-06-15 10:36:11 +0900

    3-4. AppConfig 리팩터링

--- src/main/java/hello/core1/AppConfig.java
+++ src/main/java/hello/core1/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core1;
 
+import hello.core1.discount.DiscountPolicy;
 import hello.core1.discount.FixDiscountPolicy;
+import hello.core1.member.MemberRepository;
 import hello.core1.member.MemberService;
 import hello.core1.member.MemberServiceImpl;
 import hello.core1.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import hello.core1.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
