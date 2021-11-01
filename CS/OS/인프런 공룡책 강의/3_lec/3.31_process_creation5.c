95a57d7ea71ff9cf7295dde3b4eccbc66c1927d3
Jungmin-Seo0527 <soato1405@naver.com>
2021-11-01 22:43:56 +0900

    OS 강의: 4강 프로세스의 생성
    복습 요망!!!

---
+++ CS/OS/인프런 공룡책 강의/3_lec/3.31_process_creation5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <wait.h>
+
+int value = 5;
+
+int main()
+{
+    int i;
+    pid_t pid;
+    for (i = 0; i < 3; i++)
+    {
+        pid = fork();
+        value += 5;
+        printf("Hello, fork()! pid = %d, value = %d\n", pid, value);
+    }
+    // printf("Hello, fork()! pid = %d, value = %d\n", pid, value);
+    return 0;
+}
